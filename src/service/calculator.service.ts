1a4bb4755c447be7fe3d7d11da8cdd4e2b60fd14
SantiagoComas07 <santiagocomas07d@gmail.com>
2026-01-13 21:41:48 -0500

    Math logic implemented

--- src/service/calculator.service.ts
+++ src/service/calculator.service.ts
@@ -1,8 +1,12 @@
 
 
 // DOM
+import MathLogic from "../Logic/math.logic.ts";
+
 export const calculatorService = () =>{
 
+
+    const mathLogic = new MathLogic();
 
 
 const screenDom = document.getElementById('screen');
@@ -11,13 +15,13 @@ const buttons = document.querySelectorAll('.button');
 console.log(screenDom)
 buttons.forEach((button)=>{
     console.log( 'id:', button.id);
-    button.addEventListener(('click'), (e)=>{
-        console.log(e)
+    button.addEventListener(('click'), ()=>{
+
 
         // Condition delete
         const contentButton = button.textContent;
         //Test
-        console.log(contentButton);
+        // console.log(contentButton);
 
 
 
@@ -40,10 +44,50 @@ buttons.forEach((button)=>{
                 return;
 
             }
+
+
+            if(screenDom.textContent === '0' || screenDom.textContent === 'ERROR'){
+                screenDom.textContent = contentButton;
+            }else{
+                    screenDom.textContent += contentButton;
+
+            }
+
+
+
             // Equal condition
             if(button.id === 'equal'){
                 try {
-                    screenDom.textContent = eval(screenDom.textContent);
+
+                        let data = screenDom.textContent;
+                        data = data.replace(/\s+/g, '').replace('=', '');
+                        const operator = data.match(/[+\-*/]/)?.[0];
+                        if (!operator) return;
+                        const [a, b] = data.split(operator);
+                        if (!a || !b) {
+                            screenDom.textContent = 'ERROR';
+                            return;
+                        }
+                        let number1 = Number(a);
+                        let number2 = Number(b);
+                        let result;
+                        // Operador (+,-,*,/)
+                    switch (operator) {
+                        case '+':
+                            result = mathLogic.addition(number1, number2);
+                            break;
+                        case '-':
+                            result = mathLogic.subtraction(number1, number2);
+                            break;
+                        case '*':
+                            result = mathLogic.multiplication(number1, number2);
+                            break;
+                        case '/':
+                            if (number2 === 0) throw new Error('Division by zero');
+                            result = mathLogic.division(number1, number2);
+                            break;
+                    }
+                    screenDom.textContent = String(result);
 
                 }catch(error){
                     console.error(error);
@@ -51,15 +95,6 @@ buttons.forEach((button)=>{
 
                 }
             }
-
-            if(screenDom.textContent === '0' || screenDom.textContent === 'ERROR'){
-                screenDom.textContent = contentButton;
-            }else{
-                screenDom.textContent += contentButton;
-            }
-
-
-
 
         }
 
